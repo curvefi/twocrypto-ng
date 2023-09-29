b688a811b1552f299a3d7b461d18dba380341757
bout3fiddy <11488427+bout3fiddy@users.noreply.github.com>
2023-09-29 14:47:00 +0200

    add: contracts

---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+pytest_plugins = [
+    "tests.fixtures.accounts",
+    "tests.fixtures.tokens",
+    "tests.fixtures.functions",
+    "tests.fixtures.pool",
+    "tests.fixtures.factory",
+]
---
+++ tests/fixtures/tokens.py
@@ -0,0 +1,64 @@
+import boa
+import pytest
+
+
+@pytest.fixture(scope="module")
+def weth(deployer):
+    with boa.env.prank(deployer):
+        return boa.load("contracts/mocks/WETH.vy")
+
+
+@pytest.fixture(scope="module")
+def usd(deployer):
+    with boa.env.prank(deployer):
+        return boa.load("contracts/mocks/ERC20Mock.vy", "USD", "USD", 18)
+
+
+@pytest.fixture(scope="module")
+def btc(deployer):
+    with boa.env.prank(deployer):
+        return boa.load("contracts/mocks/ERC20Mock.vy", "BTC", "BTC", 18)
+
+
+@pytest.fixture(scope="module")
+def wbtc(deployer):
+    with boa.env.prank(deployer):
+        return boa.load("contracts/mocks/ERC20Mock.vy", "BTC", "BTC", 8)
+
+
+@pytest.fixture(scope="module")
+def usdt(deployer):
+    with boa.env.prank(deployer):
+        return boa.load("contracts/mocks/ERC20Mock.vy", "USDT", "USDT", 6)
+
+
+@pytest.fixture(scope="module")
+def usdc(deployer):
+    with boa.env.prank(deployer):
+        return boa.load("contracts/mocks/ERC20Mock.vy", "USDC", "USDC", 6)
+
+
+@pytest.fixture(scope="module")
+def dai(deployer):
+    with boa.env.prank(deployer):
+        return boa.load("contracts/mocks/ERC20Mock.vy", "DAI", "DAI", 18)
+
+
+@pytest.fixture(scope="module")
+def coins(usd, btc, weth):
+    yield [usd, btc, weth]
+
+
+@pytest.fixture(scope="module")
+def twocrypto_coins(usdt, wbtc, weth):
+    yield [usdt, weth]
+
+
+@pytest.fixture(scope="module")
+def stablecoins(usdc, usdt, dai):
+    yield [dai, usdc, usdt]
+
+
+@pytest.fixture(scope="module")
+def pool_coins(coins):
+    yield coins
