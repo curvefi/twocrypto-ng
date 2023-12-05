9ae0c8553952118b652aeffeb613338455f357f5
Filipp <>
2023-12-05 09:48:32 +0300

    Update newton_D test.

--- tests/unitary/math/test_newton_D.py
+++ tests/unitary/math/test_newton_D.py
@@ -2,16 +2,15 @@
 import sys
 import time
 from decimal import Decimal
-
 import pytest
 from boa.vyper.contract import BoaError
 from hypothesis import given, settings
 from hypothesis import strategies as st
-
 import tests.utils.simulation_int_many as sim
 
-sys.stdout = sys.stderr
 
+# Uncomment to be able to print when parallelized
+# sys.stdout = sys.stderr
 
 def inv_target_decimal_n2(A, gamma, x, D):
     N = len(x)
@@ -37,7 +36,8 @@ def inv_target_decimal_n2(A, gamma, x, D):
 
 
 N_COINS = 2
-MAX_SAMPLES = 3000000  # Increase for fuzzing
+MAX_SAMPLES = 1000000  # Increase for fuzzing
+N_CASES = 1
 
 A_MUL = 10000
 MIN_A = int(N_COINS**N_COINS * A_MUL / 10)
@@ -47,27 +47,26 @@ MAX_A = int(N_COINS**N_COINS * A_MUL * 100000)
 MIN_GAMMA = 10**10
 MAX_GAMMA = 5 * 10**16
 
-MIN_XD = 10**16 - 1
-MAX_XD = 10**20 + 1
+MIN_XD = 10**17
+MAX_XD = 10**19
 
 pytest.progress = 0
-pytest.positive_dy = 0
+pytest.actually_tested = 0
 pytest.t_start = time.time()
-pytest.gas_original = 0
-pytest.gas_new = 0
-failed_cases = []
 
-
+@pytest.mark.parametrize(
+            "_tmp", range(N_CASES)
+                )  # Create N_CASES independent test instances.
 @given(
     A=st.integers(min_value=MIN_A, max_value=MAX_A),
     D=st.integers(
         min_value=10**18, max_value=10**14 * 10**18
     ),  # 1 USD to 100T USD
     xD=st.integers(
-        min_value=int(1.001e16), max_value=int(0.999e20)
+        min_value=MIN_XD, max_value=MAX_XD
     ),  # <- ratio 1e18 * x/D, typically 1e18 * 1
     yD=st.integers(
-        min_value=int(1.001e16), max_value=int(0.999e20)
+        min_value=MIN_XD, max_value=MAX_XD
     ),  # <- ratio 1e18 * y/D, typically 1e18 * 1
     gamma=st.integers(min_value=MIN_GAMMA, max_value=MAX_GAMMA),
     j=st.integers(min_value=0, max_value=1),
@@ -99,6 +98,7 @@ def test_newton_D(
     mid_fee,
     out_fee,
     fee_gamma,
+    _tmp,
 ):
     _test_newton_D(
         math_optimized,
@@ -114,6 +114,7 @@ def test_newton_D(
         mid_fee,
         out_fee,
         fee_gamma,
+        _tmp,
     )
 
 
@@ -131,6 +132,7 @@ def _test_newton_D(
     mid_fee,
     out_fee,
     fee_gamma,
+    _tmp,
 ):
 
     is_safe = all(
@@ -139,11 +141,8 @@ def _test_newton_D(
     )
 
     pytest.progress += 1
-    if pytest.progress % 1000 == 0 and pytest.positive_dy != 0:
-        print(
-            f"{pytest.progress} | {pytest.positive_dy} cases processed in {time.time()-pytest.t_start:.1f} seconds."
-            f"Gas advantage per call: {pytest.gas_original//pytest.positive_dy} {pytest.gas_new//pytest.positive_dy}\n"
-        )
+    if pytest.progress % 1000 == 0 and pytest.actually_tested != 0:
+        print(f"{pytest.progress} | {pytest.actually_tested} cases processed in {time.time()-pytest.t_start:.1f} seconds.")
     X = [D * xD // 10**18, D * yD // 10**18]
 
     result_get_y = 0
@@ -156,7 +155,7 @@ def _test_newton_D(
     if get_y_failed:
         newton_y_failed = False
         try:
-            math_optimized.internal._newton_y(A, gamma, X, D, j)
+            math_optimized.newton_y(A, gamma, X, D, j)
         except:
             newton_y_failed = True
 
@@ -183,58 +182,35 @@ def _test_newton_D(
 
         if dy / X[j] <= 0.95:
 
-            pytest.positive_dy += 1
+            pytest.actually_tested += 1
             X[j] = y
 
+            case = (
+                "{"
+                f"'ANN': {A}, 'D': {D}, 'xD': {xD}, 'yD': {yD}, 'GAMMA': {gamma}, 'j': {j}, 'btcScalePrice': {btcScalePrice}, 'ethScalePrice': {ethScalePrice}, 'mid_fee': {mid_fee}, 'out_fee': {out_fee}, 'fee_gamma': {fee_gamma}"
+                "},\n"
+            )
+
+            result_sim = math_unoptimized.newton_D(A, gamma, X)
             try:
-                result_sim = math_unoptimized.newton_D(A, gamma, X)
-            except:
-                raise  # this is a problem
+                result_contract = math_optimized.newton_D(A, gamma, X, K0)
+            except Exception as e:
+                # with open("log/newton_D_fail.txt", "a") as f:
+                #     f.write(case)
+                # with open("log/newton_D_fail_trace.txt", "a") as f:
+                #     f.write(str(e))
+                    return
 
-            try:
-                result_sim = math_unoptimized.newton_D(A, gamma, X)
-                pytest.gas_original += (
-                    math_unoptimized._computation.get_gas_used()
-                )
-                try:
-                    result_contract = math_optimized.newton_D(A, gamma, X, K0)
-                    pytest.gas_new += (
-                        math_optimized._computation.get_gas_used()
-                    )
-                except BoaError as e:
-                    # print(e)
-                    case = (
-                        "{"
-                        f"'ANN': {A}, 'D': {D}, 'xD': {xD}, 'yD': {yD}, 'GAMMA': {gamma}, 'j': {j}, 'btcScalePrice': {btcScalePrice}, 'ethScalePrice': {ethScalePrice}, 'mid_fee': {mid_fee}, 'out_fee': {out_fee}, 'fee_gamma': {fee_gamma}"
-                        "},\n"
-                    )
-                    print(case)
-                    raise
+            A_dec = Decimal(A) / 10000 / 4
 
-                A_dec = Decimal(A) / 10000 / 4
+            def calculate_D_polynome(d):
+                d = Decimal(d)
+                return abs(inv_target_decimal_n2(A_dec, gamma, X, d))
 
-                def calculate_D_polynome(d):
-                    d = Decimal(d)
-                    return abs(inv_target_decimal_n2(A_dec, gamma, X, d))
+            assert abs(result_sim - result_contract) <= max(
+                10000, result_sim / 1e12
+            )
 
-                # print(f"ANN={A}; GAMMA={gamma}; x={X}")
+            # with open("log/newton_D_pass.txt", "a") as f:
+            #     f.write(case)
 
-                # D0 = int(2 * (X[0]*X[1])**(Decimal(1)/2))
-                # D0_new = int((10**18*4*X[0]*X[1]//K0)**(Decimal(1)/2))
-                # print(math_unoptimized._computation.get_gas_used(), D0, calculate_D_polynome(D0))
-                # print(math_optimized._computation.get_gas_used(), D0_new, calculate_D_polynome(D0_new))
-
-                try:
-                    assert abs(result_sim - result_contract) <= max(
-                        10000, result_sim / 1e12
-                    )
-                except AssertionError:
-                    case = (
-                        "{"
-                        f"'ANN': {A}, 'D': {D}, 'xD': {xD}, 'yD': {yD}, 'GAMMA': {gamma}, 'j': {j}, 'btcScalePrice': {btcScalePrice}, 'ethScalePrice': {ethScalePrice}, 'mid_fee': {mid_fee}, 'out_fee': {out_fee}, 'fee_gamma': {fee_gamma}"
-                        "},\n"
-                    )
-                    with open("newton_D_n=2_cases.txt", "a") as f:
-                        f.write(case)
-            except:
-                raise
