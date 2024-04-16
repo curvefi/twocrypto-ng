05e7a8a62ea98232dd0c4245fa9e5ee52d96fadf
Alberto <albicento.ac@gmail.com>
2024-04-16 11:35:17 +0200

    test: ensuring old bounds behavior is unchanged

--- tests/unitary/math/test_newton_y.py
+++ tests/unitary/math/test_newton_y.py
@@ -1,6 +1,6 @@
 import boa
 import pytest
-from hypothesis import given, settings
+from hypothesis import event, given, settings
 from hypothesis import strategies as st
 
 N_COINS = 2
@@ -12,8 +12,10 @@ A_MUL = 10000
 MIN_A = int(N_COINS**N_COINS * A_MUL / 10)
 MAX_A = int(N_COINS**N_COINS * A_MUL * 1000)
 
-MIN_GAMMA = 10**10
-MAX_GAMMA = 3 * 10**17
+# Old bounds for gamma
+# should be used only when comparing convergence with the old version
+MIN_GAMMA_CMP = 10**10
+MAX_GAMMA_CMP = 2 * 10**15
 
 
 @pytest.fixture(scope="module")
@@ -23,7 +25,7 @@ def math_large_gamma():
 
 @pytest.fixture(scope="module")
 def math_small_gamma():
-    return boa.load("contracts/mock/newton_y_small_gamma.vy")
+    return boa.load("contracts/mocks/newton_y_small_gamma.vy")
 
 
 @given(
@@ -37,17 +39,25 @@ def math_small_gamma():
     yD=st.integers(
         min_value=10**17 // 2, max_value=10**19 // 2
     ),  # <- ratio 1e18 * y/D, typically 1e18 * 1
-    gamma=st.integers(min_value=MIN_GAMMA, max_value=MAX_GAMMA),
+    gamma=st.integers(min_value=MIN_GAMMA_CMP, max_value=MAX_GAMMA_CMP),
     j=st.integers(min_value=0, max_value=1),
 )
 @settings(max_examples=MAX_SAMPLES, deadline=None)
-def test_iteration_diff(math_large_gamma, A, D, xD, yD, gamma, j):
-    pass
-    # TODO: make a test that:
-    # - measures how many iterations it takes for the
-    #   old value to converge between the two versions
-    # - makes sure that we're converging to the correct value
-    # - use hypothesis.event to have some clear statistics about
-    #   the differences in divergence
-    # X = [D * xD // 10**18, D * yD // 10**18]
-    # math_large_gamma.newton_y(A, gamma, X, D, j)
+def test_newton_y_equivalence(
+    math_small_gamma, math_large_gamma, A, D, xD, yD, gamma, j
+):
+    """
+    Tests whether the newton_y function converges to the same
+    value for both the old and new versions
+    """
+    X = [D * xD // 10**18, D * yD // 10**18]
+    y_small, iterations_old = math_small_gamma.newton_y(A, gamma, X, D, j)
+    y_large, iterations_new = math_large_gamma.newton_y(A, gamma, X, D, j)
+
+    # print(math_large_gamma.internal._newton_y)
+
+    event(f"converges in {iterations_new} iterations")
+
+    # create events depending on the differences between iterations
+    assert iterations_old - iterations_new == 0
+    assert y_small == y_large
