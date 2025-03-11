6933bad3da067e9455e7e9e447464b7f7639439c
Alberto <albicento.ac@gmail.com>
2025-03-11 13:05:01 +0100

    test: fully separate unitary from fuzzing

--- tests/fuzzing/test_deposit_withdraw.py
+++ tests/fuzzing/test_deposit_withdraw.py
@@ -1,5 +1,4 @@
 import boa
-import pytest
 from boa.test import strategy
 from hypothesis import given, settings
 
@@ -13,71 +12,6 @@ SETTINGS = {"max_examples": 100, "deadline": None}
 
 def assert_string_contains(string, substrings):
     assert any(substring in string for substring in substrings)
-
-
-@pytest.fixture(scope="module")
-def test_1st_deposit_and_last_withdraw(swap, coins, user, fee_receiver):
-    quantities = [10**36 // p for p in INITIAL_PRICES]  # $3M worth
-
-    for coin, q in zip(coins, quantities):
-        mint_for_testing(coin, user, q)
-        with boa.env.prank(user):
-            coin.approve(swap, 2**256 - 1)
-
-    bal_before = boa.env.get_balance(swap.address)
-    with boa.env.prank(user):
-        swap.add_liquidity(quantities, 0)
-
-    # test if eth wasnt deposited:
-    assert boa.env.get_balance(swap.address) == bal_before
-
-    token_balance = swap.balanceOf(user)
-    assert token_balance == swap.totalSupply() - swap.balanceOf(fee_receiver) > 0
-    assert abs(swap.get_virtual_price() / 1e18 - 1) < 1e-3
-
-    # Empty the contract
-    with boa.env.prank(user):
-        swap.remove_liquidity(token_balance, [0] * len(coins))
-
-    # nothing is left except admin balances
-    assert swap.balanceOf(user) == swap.totalSupply() - swap.balanceOf(fee_receiver) == 0
-
-    return swap
-
-
-@pytest.fixture(scope="module")
-def test_first_deposit_full_withdraw_second_deposit(
-    test_1st_deposit_and_last_withdraw, user, coins, fee_receiver
-):
-    swap = test_1st_deposit_and_last_withdraw
-    quantities = [10**36 // p for p in INITIAL_PRICES]  # $2M worth
-
-    for coin, q in zip(coins, quantities):
-        mint_for_testing(coin, user, q)
-        with boa.env.prank(user):
-            coin.approve(swap, 2**256 - 1)
-
-    # Second deposit
-    eth_bal_before = boa.env.get_balance(swap.address)
-    swap_balances_before = [swap.balances(i) for i in range(3)]
-    with boa.env.prank(user):
-        swap.add_liquidity(quantities, 0)
-
-    assert swap.xcp_profit_a() >= 10**18
-    assert swap.xcp_profit() >= 10**18
-    assert swap.virtual_price() >= 10**18
-
-    # test if eth was not deposited:
-    assert boa.env.get_balance(swap.address) == eth_bal_before
-
-    for i in range(len(coins)):
-        assert swap.balances(i) == quantities[i] + swap_balances_before[i]
-
-    token_balance = swap.balanceOf(user)
-    assert token_balance + swap.balanceOf(fee_receiver) == swap.totalSupply() > 0
-    assert abs(swap.get_virtual_price() / 1e18 - 1) < 1e-3
-
-    return swap
 
 
 @given(
---
+++ tests/fuzzing/test_exchange_fuzzing.py
@@ -0,0 +1,128 @@
+import boa
+from boa.test import strategy
+from hypothesis import given, settings  # noqa
+
+from tests.fixtures.pool import INITIAL_PRICES
+from tests.utils.tokens import mint_for_testing
+
+SETTINGS = {"max_examples": 100, "deadline": None}
+
+
+@given(
+    amount=strategy(
+        "uint256", min_value=10**10, max_value=10**6 * 10**18
+    ),  # Can be more than we have
+    i=strategy("uint", min_value=0, max_value=1),
+    j=strategy("uint", min_value=0, max_value=1),
+)
+@settings(**SETTINGS)
+def test_exchange_all(
+    swap_with_deposit,
+    views_contract,
+    coins,
+    user,
+    amount,
+    i,
+    j,
+):
+    if i == j:
+        return
+
+    amount = amount * 10**18 // INITIAL_PRICES[i]
+    mint_for_testing(coins[i], user, amount)
+
+    calculated = views_contract.get_dy(i, j, amount, swap_with_deposit)
+
+    measured_i = coins[i].balanceOf(user)
+    measured_j = coins[j].balanceOf(user)
+    d_balance_i = swap_with_deposit.balances(i)
+    d_balance_j = swap_with_deposit.balances(j)
+
+    with boa.env.prank(user):
+        swap_with_deposit.exchange(i, j, amount, int(0.999 * calculated))
+
+    measured_i -= coins[i].balanceOf(user)
+    measured_j = coins[j].balanceOf(user) - measured_j
+    d_balance_i = swap_with_deposit.balances(i) - d_balance_i
+    d_balance_j = swap_with_deposit.balances(j) - d_balance_j
+
+    assert amount == measured_i
+    assert calculated == measured_j
+
+    assert d_balance_i == amount
+    assert -d_balance_j == measured_j
+
+
+@given(
+    amount=strategy(
+        "uint256", min_value=10**10, max_value=10**6 * 10**18
+    ),  # Can be more than we have
+    i=strategy("uint", min_value=0, max_value=1),
+    j=strategy("uint", min_value=0, max_value=1),
+)
+@settings(**SETTINGS)
+def test_exchange_received_success(
+    swap_with_deposit,
+    views_contract,
+    coins,
+    user,
+    amount,
+    i,
+    j,
+):
+    if i == j:
+        return
+
+    amount = amount * 10**18 // INITIAL_PRICES[i]
+    mint_for_testing(coins[i], user, amount)
+
+    calculated = views_contract.get_dy(i, j, amount, swap_with_deposit)
+
+    measured_i = coins[i].balanceOf(user)
+    measured_j = coins[j].balanceOf(user)
+    d_balance_i = swap_with_deposit.balances(i)
+    d_balance_j = swap_with_deposit.balances(j)
+
+    with boa.env.prank(user):
+        coins[i].transfer(swap_with_deposit, amount)
+        out = swap_with_deposit.exchange_received(i, j, amount, int(0.999 * calculated), user)
+
+    measured_i -= coins[i].balanceOf(user)
+    measured_j = coins[j].balanceOf(user) - measured_j
+    d_balance_i = swap_with_deposit.balances(i) - d_balance_i
+    d_balance_j = swap_with_deposit.balances(j) - d_balance_j
+
+    assert amount == measured_i
+    assert calculated == measured_j == out
+
+    assert d_balance_i == amount
+    assert -d_balance_j == measured_j == out
+
+
+@given(
+    amount=strategy(
+        "uint256", min_value=10**10, max_value=10**6 * 10**18
+    ),  # Can be more than we have
+    i=strategy("uint", min_value=0, max_value=1),
+    j=strategy("uint", min_value=0, max_value=1),
+)
+@settings(**SETTINGS)
+def test_exchange_received_revert_on_no_transfer(
+    swap_with_deposit,
+    views_contract,
+    coins,
+    user,
+    amount,
+    i,
+    j,
+):
+    if i == j:
+        return
+
+    amount = amount * 10**18 // INITIAL_PRICES[i]
+    mint_for_testing(coins[i], user, amount)
+
+    calculated = views_contract.get_dy(i, j, amount, swap_with_deposit)
+
+    with boa.env.prank(user), boa.reverts("user didn't give us coins"):
+        swap_with_deposit.exchange_received(i, j, amount, int(0.999 * calculated), user)
