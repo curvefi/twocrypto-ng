cd48d0dac17f1f40bbbcd9ed765bcecfd0138946
Alberto <albicento.ac@gmail.com>
2025-03-26 16:23:07 +0100

    test: improve god mode

--- tests/utils/god_mode.py
+++ tests/utils/god_mode.py
@@ -4,20 +4,41 @@ the amount of boilerplate code needed to write meaningful tests.
 """
 
 import boa
-from tests.utils.constants import N_COINS
+from tests.utils.constants import N_COINS, ERC20_DEPLOYER
 
 god = boa.env.generate_address()
 
 
 class GodModePool:
-    def __init__(self, instance, coins):
+    def __init__(self, instance):
         self.instance = instance
-        self.coins = coins
+        self.coins = [ERC20_DEPLOYER.at(instance.coins(i)) for i in range(N_COINS)]
         for c in self.coins:
             c.approve(self.instance, 2**256 - 1, sender=god)
 
+    def compute_balanced_amounts(self, amount):
+        return [amount, amount * 10**18 // self.instance.price_scale()]
+
     def __getattr__(self, name):
         return getattr(self.instance, name)
+
+    def coins(self, i=None):
+        if i is None:
+            return self.coins
+        return self.coins[i]
+
+    def donate(self, amounts, slippage=0, update_ema=False):
+        self.__premint_amounts(amounts)
+
+        self.instance.donate(amounts, slippage, sender=god)
+
+        if update_ema:
+            self.__update_ema()
+
+    def donate_balanced(self, amount, update_ema=False):
+        balanced_amounts = self.compute_balanced_amounts(amount)
+
+        return self.donate(balanced_amounts, update_ema=update_ema)
 
     def exchange(self, i, dx, update_ema=False):
         if i == 0:
@@ -44,7 +65,7 @@ class GodModePool:
         return lp_tokens_received
 
     def add_liquidity_balanced(self, amount, update_ema=False):
-        balanced_amounts = [amount, amount * 10**18 // self.instance.price_scale()]
+        balanced_amounts = self.compute_balanced_amounts(amount)
 
         return self.add_liquidity(balanced_amounts, update_ema=update_ema)
 
