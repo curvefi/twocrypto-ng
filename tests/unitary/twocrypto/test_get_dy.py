41dec626e0d9262309187a4fc7e6918a530e31cb
heswithme <heswithme@pm.me>
2025-07-11 13:18:46 +0200

    chore: rm comments

--- tests/unitary/twocrypto/test_get_dy.py
+++ tests/unitary/twocrypto/test_get_dy.py
@@ -2,19 +2,6 @@ from tests.utils.god_mode import GodModePool
 from tests.utils.constants import N_COINS
 import pytest
 
-# @external
-# @view
-# def get_dy(i: uint256, j: uint256, dx: uint256) -> uint256:
-#     """
-#     @notice Get amount of coin[j] tokens received for swapping in dx amount of coin[i]
-#     @dev Includes fee.
-#     @param i index of input token. Check pool.coins(i) to get coin address at ith index
-#     @param j index of output token
-#     @param dx amount of input coin[i] tokens
-#     @return uint256 Exact amount of output j tokens for dx amount of i input tokens.
-#     """
-#     return staticcall self.view_contract.get_dy(i, j, dx, self)
-
 
 @pytest.mark.parametrize("i", range(N_COINS))
 @pytest.mark.parametrize("dx_ratio", range(1, 10))
