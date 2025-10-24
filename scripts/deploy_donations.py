83682b00658b0e9a86b2746958c08c6e97c764f2
heswithme <heswithme@pm.me>
2025-10-24 17:51:32 +0200

    feat: bsc & base deployments

--- scripts/deploy_donations.py
+++ scripts/deploy_donations.py
@@ -4,6 +4,7 @@ from eth_account import Account
 from boa.explorer import Etherscan
 from secure_key_utils import decrypt_private_key, getpass
 import time
+from eth_utils import keccak
 
 # deploy as blueprints
 DEPLOY = False
@@ -29,7 +30,8 @@ def twocrypto_with_periphery(twocrypto_path, views_address, math_address, admin_
 
 
 # rpc_url = "https://bsc-dataseed.bnbchain.org"
-rpc_url = "https://eth.drpc.org"
+# rpc_url = "https://eth.drpc.org"
+rpc_url = "https://mainnet.base.org"
 etherscan_api_key = os.environ.get("ETHERSCAN_API_KEY")
 
 # private_key = os.environ.get("WEB3_TESTNET_PK")
@@ -59,19 +61,24 @@ math_deployer = boa.load_partial(math_path)
 views_deployer = boa.load_partial(views_path)
 
 if DEPLOY:
-    # print("Deploying math contract...")
-    # math_contract = math_deployer.deploy()
-    # time.sleep(5)
-    # print("Deploying views contract...")
-    # views_contract = views_deployer.deploy()
-    # time.sleep(5)
-    # math_address = math_contract.address
-    # views_address = views_contract.address
-    math_address = "0x79839c2D74531A8222C0F555865aAc1834e82e51"  # eth
-    views_address = "0x35048188c02cbc9239e1e5ecb3761eF9dfDcD31f"  # eth
-
-    # math_address = "0xd908A6ed4DCE4139f9b0F0E9c6c769539a9D7601" #bsc
-    # views_address = "0x068712A87FFCB06cd1069Ad7526bDA8Bd564A910" #bsc
+    if boa.env.evm.patch.chain_id == 1:
+        math_address = "0x79839c2D74531A8222C0F555865aAc1834e82e51"  # eth
+        views_address = "0x35048188c02cbc9239e1e5ecb3761eF9dfDcD31f"  # eth
+    elif boa.env.evm.patch.chain_id == 56:
+        math_address = "0xd908A6ed4DCE4139f9b0F0E9c6c769539a9D7601"  # bsc
+        views_address = "0x068712A87FFCB06cd1069Ad7526bDA8Bd564A910"  # bsc
+    elif boa.env.evm.patch.chain_id == 8453:
+        math_address = "0x2Bd498ae431dC98694010950fcF8ACd3599f5512"
+        views_address = "0xFcBA2D0133F705DD8bAf250a64f1DE0d7091F5Bd"  # base
+    else:
+        print("Deploying math contract...")
+        math_contract = math_deployer.deploy()
+        time.sleep(5)
+        print("Deploying views contract...")
+        views_contract = views_deployer.deploy()
+        time.sleep(5)
+        math_address = math_contract.address
+        views_address = views_contract.address
     math_contract = math_deployer.at(math_address)
     views_contract = views_deployer.at(views_address)
     print("Deploying twocrypto contract...")
@@ -81,10 +88,21 @@ if DEPLOY:
     twocrypto_contract = twocrypto_deployer.deploy_as_blueprint()
     time.sleep(5)
 else:
-    math_address = "0x79839c2D74531A8222C0F555865aAc1834e82e51"
-    views_address = "0x35048188c02cbc9239e1e5ecb3761eF9dfDcD31f"
-    twocrypto_address = "0xD1FAeCA80d6FDd1DF4CBcCe4b2551b6Ee63Ae3D6"
+    if boa.env.evm.patch.chain_id == 1:
+        math_address = "0x79839c2D74531A8222C0F555865aAc1834e82e51"  # eth
+        views_address = "0x35048188c02cbc9239e1e5ecb3761eF9dfDcD31f"  # eth
+        twocrypto_address = "0xD1FAeCA80d6FDd1DF4CBcCe4b2551b6Ee63Ae3D6"
 
+    elif boa.env.evm.patch.chain_id == 56:
+        math_address = "0xd908A6ed4DCE4139f9b0F0E9c6c769539a9D7601"  # bsc
+        views_address = "0x068712A87FFCB06cd1069Ad7526bDA8Bd564A910"  # bsc
+        twocrypto_address = "0xbe365a090321E0E012f448B42feDfB74A7Ea4d9D"
+    elif boa.env.evm.patch.chain_id == 8453:
+        math_address = "0x2Bd498ae431dC98694010950fcF8ACd3599f5512"
+        views_address = "0xFcBA2D0133F705DD8bAf250a64f1DE0d7091F5Bd"  # base
+        twocrypto_address = "0x56545b4640e5f0937e56843ad8f0a3cd44fc0785"
+    else:
+        raise ValueError(f"Chain ID {boa.env.evm.patch.chain_id} not supported")
     math_contract = math_deployer.at(math_address)
     views_contract = views_deployer.at(views_address)
     twocrypto_deployer = twocrypto_with_periphery(
@@ -113,11 +131,6 @@ for contract in [math_contract, views_contract, twocrypto_contract]:
 # print(factory.admin())
 
 # set pool implementation
-# pool_id = int(keccak(text="fx").hex(), 16)
-# print("Implementation ID:", pool_id)
+pool_id = int(keccak(text="fx50").hex(), 16)
+print("Implementation ID:", pool_id)
 # factory.set_pool_implementation(twocrypto_contract.address, pool_id, sender=deployer.address)
-
-# set pool periphery
-# pool = boa.from_etherscan("0xC69A9c962A65a5E11D38A0f23050D5Ac319d18DD")
-# pool.set_periphery(views_contract.address, math_contract.address, sender=deployer.address)
-# pool.set_admin_fee(2500000000, sender=deployer.address)
