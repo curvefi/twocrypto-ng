d4d5975e504cf6604a1a4792f73d83573044ec34
heswithme <heswithme@pm.me>
2026-04-09 12:28:24 +0200

    feat: lpf&admin_fee setter

---
+++ tests/unitary/twocrypto/test_set_donation_parameters.py
@@ -0,0 +1,51 @@
+import boa
+import pytest
+from tests.utils.constants import FACTORY_DEPLOYER, PRECISION
+
+
+def test_default_behavior(pool):
+    duration = 100
+    period = 120
+    threshold = 30 * PRECISION // 100
+    max_shares_ratio = 15 * PRECISION // 100
+
+    pool.set_donation_parameters(
+        duration,
+        period,
+        threshold,
+        max_shares_ratio,
+        sender=FACTORY_DEPLOYER.at(pool.factory()).admin(),
+    )
+
+    logs = pool.get_logs()
+    assert len(logs) == 1
+    assert type(logs[0]).__name__ == "SetDonationParameters"
+    assert logs[0].duration == duration
+    assert logs[0].donation_protection_period == period
+    assert logs[0].donation_protection_lp_threshold == threshold
+    assert logs[0].donation_shares_max_ratio == max_shares_ratio
+
+    assert pool.donation_duration() == duration
+    assert pool.donation_protection_period() == period
+    assert pool.donation_protection_lp_threshold() == threshold
+    assert pool.donation_shares_max_ratio() == max_shares_ratio
+
+
+def test_only_owner(pool):
+    with boa.reverts("only owner"):
+        pool.set_donation_parameters(100, 120, 1, 1)
+
+
+@pytest.mark.parametrize(
+    "duration,period,threshold,max_shares_ratio",
+    [(0, 1, 1, 1), (1, 0, 1, 1), (1, 1, 0, 1), (1, 1, 1, 0)],
+)
+def test_invalid_params(pool, duration, period, threshold, max_shares_ratio):
+    with boa.reverts():
+        pool.set_donation_parameters(
+            duration,
+            period,
+            threshold,
+            max_shares_ratio,
+            sender=FACTORY_DEPLOYER.at(pool.factory()).admin(),
+        )
---
+++ tests/unitary/twocrypto/test_set_fee_parameters.py
@@ -0,0 +1,34 @@
+import boa
+import pytest
+
+from tests.utils.constants import PRECISION
+
+
+@pytest.mark.parametrize("admin_fee", [int(i * 10**10 / 4) for i in range(5)])
+@pytest.mark.parametrize("lp_profit_fraction", [0, PRECISION // 2, PRECISION])
+def test_default_behavior(pool, factory_admin, admin_fee, lp_profit_fraction):
+    pool.set_fee_parameters(admin_fee, lp_profit_fraction, sender=factory_admin)
+
+    logs = pool.get_logs()
+    assert len(logs) == 1
+    assert type(logs[0]).__name__ == "SetFeeParameters"
+    assert logs[0].admin_fee == admin_fee
+    assert logs[0].lp_profit_fraction == lp_profit_fraction
+
+    assert pool.admin_fee() == admin_fee
+    assert pool.lp_profit_fraction() == lp_profit_fraction
+
+
+def test_only_owner(pool):
+    with boa.reverts("only owner"):
+        pool.set_fee_parameters(0, 0)
+
+
+def test_admin_fee_greater_than_max(pool, factory_admin):
+    with boa.reverts():
+        pool.set_fee_parameters(10**10 + 1, 0, sender=factory_admin)
+
+
+def test_lp_profit_fraction_greater_than_precision(pool, factory_admin):
+    with boa.reverts():
+        pool.set_fee_parameters(0, PRECISION + 1, sender=factory_admin)
